3529af129fbac045a31b2c92254fedc228fcd314
JEYASEELAN-2399 <115181970+JEYASEELAN-2399@users.noreply.github.com>
2023-02-21 11:08:06 +0530

    commit

--- src/ARRAY.java
+++ src/ARRAY.java
@@ -1,29 +1,19 @@
 class arrays{
 
-public static void main(String[] args) {
-
-  /*
+   /*
    * syntax <datatype> <variable name> = new datatype<size>
    *        int[]    employeename = new int[5];
    * or 
    *        int[] employeename={"jeyaseelan","john","jeii"}
    */
 
+public static void main(String[] args) {
+
  
 
 
-
-
-
-
   
-}
-
-
-
-
-
-
+  }
 }
 
 
